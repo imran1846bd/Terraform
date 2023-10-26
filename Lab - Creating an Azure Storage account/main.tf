b2b4fd5efb994aa3a5d76c42cb40a510af30d56e
Imran Hasan <74506793+imran1846bd@users.noreply.github.com>
2023-10-26 17:18:13 +0600

    Add files via upload

---
+++ Lab - Creating an Azure Storage account/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.77.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id = "7031074b-8de3-4a18-9242-379f4bac0778"
+    tenant_id = "40111bb2-bdde-498a-9ecd-ee261089b87e"
+    client_id = "f363f6cd-465d-46ca-91ec-9229560c8052"
+    client_secret = "SE-8Q~e4ax53GiPPjF5zcck2h46zjN1_P_ERAdj6"
+    features {}
+
+}
+resource "azurerm_resource_group" "appgrp" {
+  name     = "app-grp"
+  location = "North Europe"
+}
+
+resource "azurerm_storage_account" "appazure1846cicada" {
+  name                     = "appazure1846cicada"
+  resource_group_name      = "app-grp"
+  location                 = "North Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+
+
+}
