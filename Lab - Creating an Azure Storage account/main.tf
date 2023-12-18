0959acb7712ed5299b72a59bdcbc4cd1f5c743e8
Imran Hasan <74506793+imran1846bd@users.noreply.github.com>
2023-12-18 12:31:37 +0600

    Update main.tf

--- Lab - Creating an Azure Storage account/main.tf
+++ Lab - Creating an Azure Storage account/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-    subscription_id = "7031074b-8de3-4a18-9242-379f4bac0778"
-    tenant_id = "40111bb2-bdde-498a-9ecd-ee261089b87e"
-    client_id = "f363f6cd-465d-46ca-91ec-9229560c8052"
-    client_secret = "SE-8Q~e4ax53GiPPjF5zcck2h46zjN1_P_ERAdj6"
+    subscription_id = "7031074b-8de3-4a1"
+    tenant_id = "40111bb2-bdde-498"
+    client_id = "f363f6cd-"
+    client_secret = "SE-8Q~e4ax53Gi"
     features {}
 
 }
